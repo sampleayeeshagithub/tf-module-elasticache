08a20f7e9f03483a94a29edca7d2ce19089265bc
Mulla Ayeesha <ayeeshamulla0708@gmailcom>
2023-12-02 16:06:44 +0530

    Ansible

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ resource "aws_elasticache_replication_group" "main" {
   description                = "${var.env}-${var.component}"
   node_type                  = var.ec_node_type
   num_cache_clusters         = var.ec_node_count
-  parameter_group_name       = "default.redis6.x.cluster.on"
+  parameter_group_name       = aws_elasticache_parameter_group.main.name
   port                       = 6379
   subnet_group_name          = aws_elasticache_subnet_group.main.name
   security_group_ids         = [aws_security_group.main.id]
@@ -52,4 +52,16 @@ resource "aws_elasticache_replication_group" "main" {
   kms_key_id                 = var.kms_key_id
   engine                     = "redis"
   engine_version             = "6.2"
+}
+
+resource "aws_elasticache_cluster" "main" {
+  cluster_id                 = "${var.env}-${var.component}-cluster"
+  engine                     = "redis"
+  node_type                  = var.ec_node_type
+  num_cache_nodes            = var.ec_node_count
+  parameter_group_name       = aws_elasticache_parameter_group.main.name
+  engine_version             = "6.2"
+  port                       = 6379
+  security_group_ids         = [aws_security_group.main.id]
+  subnet_group_name          = aws_elasticache_subnet_group.main.name
 }
